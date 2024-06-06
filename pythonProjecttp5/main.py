4b9068bccace9b62709b6d48ab496f0d8427f10e
Erharde1 <143745983+Erharde1@users.noreply.github.com>
2024-06-05 21:39:25 -0400

    Update main.py

--- pythonProjecttp5/main.py
+++ pythonProjecttp5/main.py
@@ -9,21 +9,21 @@ SCREEN_TITLE = "Roche, papier, ciseaux"
 
 
 class MyGame(arcade.Window):
+    # position des images du joueur et robot
     PLAYER_IMAGE_X = (SCREEN_WIDTH / 2) - (SCREEN_WIDTH / 4)
-    PLAYER_IMAGE_Y = SCREEN_HEIGHT / 2.5
+    PLAYER_IMAGE_Y = SCREEN_HEIGHT / 1.8
     COMPUTER_IMAGE_X = (SCREEN_WIDTH / 2) * 1.5
-    COMPUTER_IMAGE_Y = SCREEN_HEIGHT / 2.5
+    COMPUTER_IMAGE_Y = SCREEN_HEIGHT / 1.8
 
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.BLACK_OLIVE)
 
-        # Redimensionner les sprites
-        self.player_sprite = arcade.Sprite("assets/player.png", scale=0.32, center_x=self.PLAYER_IMAGE_X,
+        self.player_sprite = arcade.Sprite("assets/player.png", scale=0.4, center_x=self.PLAYER_IMAGE_X,
                                            center_y=self.PLAYER_IMAGE_Y)
-        self.computer_sprite = arcade.Sprite("assets/computer.png", scale=1.5, center_x=self.COMPUTER_IMAGE_X,
+        self.computer_sprite = arcade.Sprite("assets/computer.png", scale=1.7, center_x=self.COMPUTER_IMAGE_X,
                                              center_y=self.COMPUTER_IMAGE_Y)
-
+        
         self.player_score = 0
         self.computer_score = 0
         self.player_attack_type = None
@@ -33,6 +33,22 @@ class MyGame(arcade.Window):
         self.attack_animation = None
         self.player_attack_sprite = None
         self.computer_attack_sprite = None
+        self.show_titles = True  # afficher les titres si true
+        self.create_attack_options()
+        self.computer_placeholder = arcade.SpriteSolidColor(100, 100, arcade.color.BLACK)
+        self.computer_placeholder.center_x = self.COMPUTER_IMAGE_X
+        self.computer_placeholder.center_y = self.COMPUTER_IMAGE_Y - 120
+
+    def create_attack_options(self):
+        # images d'attaque
+        self.attack_options = {
+            AttackType.ROCK: arcade.Sprite("assets/rock.png", scale=0.5),
+            AttackType.PAPER: arcade.Sprite("assets/paper.png", scale=0.5),
+            AttackType.SCISSORS: arcade.Sprite("assets/scissors.png", scale=0.5)
+        }
+        for i, (attack_type, sprite) in enumerate(self.attack_options.items()):
+            sprite.center_x = self.PLAYER_IMAGE_X + (i - 1) * 110
+            sprite.center_y = self.PLAYER_IMAGE_Y - 120
 
     def setup(self):
         self.player_score = 0
@@ -40,12 +56,15 @@ class MyGame(arcade.Window):
         self.reset_round()
 
     def reset_round(self):
+        # reset les variables pour une nouvelle partie
         self.player_attack_type = None
         self.computer_attack_type = None
         self.player_attack_chosen = False
         self.attack_animation = None
         self.player_attack_sprite = None
         self.computer_attack_sprite = None
+        for sprite in self.attack_options.values():
+            sprite.visible = True
         if self.player_score == 3 or self.computer_score == 3:
             self.game_state = GameState.GAME_OVER
         else:
@@ -53,21 +72,28 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
+        # afficher instructions + score
         self.draw_instructions()
         self.draw_scores()
+        # afficher les images du joueur et du robot
         self.player_sprite.draw()
         self.computer_sprite.draw()
-        if self.game_state == GameState.ROUND_ACTIVE and self.player_attack_chosen:
-            if self.attack_animation:
-                self.attack_animation.draw()
-            if self.player_attack_sprite:
-                self.player_attack_sprite.draw()
+
+        for sprite in self.attack_options.values():
+            sprite.draw()
+            arcade.draw_rectangle_outline(sprite.center_x, sprite.center_y, sprite.width, sprite.height,
+                                          arcade.color.RED, 2)
+        arcade.draw_rectangle_outline(self.computer_placeholder.center_x, self.computer_placeholder.center_y,
+                                      self.computer_placeholder.width, self.computer_placeholder.height,
+                                      arcade.color.RED, 2)
+        if self.game_state in [GameState.ROUND_ACTIVE, GameState.ROUND_DONE]:
             if self.computer_attack_sprite:
                 self.computer_attack_sprite.draw()
-        elif self.game_state == GameState.ROUND_DONE:
+        if self.game_state == GameState.ROUND_DONE:
             self.draw_results()
 
     def on_update(self, delta_time):
+        # changer les attaques
         if self.attack_animation:
             self.attack_animation.update(delta_time)
         if self.game_state == GameState.ROUND_ACTIVE and self.player_attack_chosen and not self.computer_attack_type:
@@ -76,52 +102,63 @@ class MyGame(arcade.Window):
             self.validate_victory()
 
     def validate_victory(self):
+        # trouver qui a gagne
         if self.player_attack_type == self.computer_attack_type:
-            self.round_result = "Draw"
+            self.round_result = "Égalité"
         elif (self.player_attack_type == AttackType.ROCK and self.computer_attack_type == AttackType.SCISSORS) or \
                 (self.player_attack_type == AttackType.PAPER and self.computer_attack_type == AttackType.ROCK) or \
                 (self.player_attack_type == AttackType.SCISSORS and self.computer_attack_type == AttackType.PAPER):
             self.player_score += 1
-            self.round_result = "Player wins"
+            self.round_result = "Le joueur a gagné!"
         else:
             self.computer_score += 1
-            self.round_result = "Computer wins"
+            self.round_result = "L'ordinateur a gagné!"
         self.game_state = GameState.ROUND_DONE
 
     def create_attack_sprites(self):
+        # images pour les attaqies
         if self.player_attack_type:
-            self.player_attack_sprite = AttackAnimation(self.player_attack_type)
+            self.player_attack_sprite = arcade.Sprite(f"assets/{self.player_attack_type.name.lower()}.png", scale=0.5)
             self.player_attack_sprite.center_x = self.PLAYER_IMAGE_X
-            self.player_attack_sprite.center_y = self.PLAYER_IMAGE_Y - 80  # Ajuste pour afficher sous l'image du joueur
+            self.player_attack_sprite.center_y = self.PLAYER_IMAGE_Y - 80
 
         if self.computer_attack_type:
-            self.computer_attack_sprite = AttackAnimation(self.computer_attack_type)
+            self.computer_attack_sprite = arcade.Sprite(f"assets/{self.computer_attack_type.name.lower()}.png",
+                                                        scale=0.5)
             self.computer_attack_sprite.center_x = self.COMPUTER_IMAGE_X
-            self.computer_attack_sprite.center_y = self.COMPUTER_IMAGE_Y - 80  # Ajuste pour afficher sous l'image du robot
+            self.computer_attack_sprite.center_y = self.COMPUTER_IMAGE_Y - 120
 
     def draw_scores(self):
-        arcade.draw_text(f"Player Score: {self.player_score}", 10, SCREEN_HEIGHT - 20, arcade.color.WHITE, 14)
-        arcade.draw_text(f"Computer Score: {self.computer_score}", SCREEN_WIDTH - 150, SCREEN_HEIGHT - 20,
-                         arcade.color.WHITE, 14)
+        # afficher les scores du robot et du joueur
+        arcade.draw_text(f"Le pointage du joueur est {self.player_score}", self.PLAYER_IMAGE_X, self.PLAYER_IMAGE_Y - 250,
+                         arcade.color.WHITE, 14, anchor_x="center")
+        arcade.draw_text(f"Le pointage de l'ordinateur est {self.computer_score}", self.COMPUTER_IMAGE_X, self.COMPUTER_IMAGE_Y - 250,
+                         arcade.color.WHITE, 14, anchor_x="center")
 
     def draw_instructions(self):
+        # afficher les instructions du jeu
+        if self.show_titles:  # Seulement afficher les titres si true
+            arcade.draw_text("Roche, papier, ciseaux", SCREEN_WIDTH // 2, SCREEN_HEIGHT - 100, arcade.color.DARK_RED, 50,
+                             anchor_x="center")
+            arcade.draw_text("Appuyer sur une image pour faire une attaque!", SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150, arcade.color.BLUE, 20,
+                             anchor_x="center")
         if self.game_state == GameState.NOT_STARTED:
-            arcade.draw_text("Press SPACE to start", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, arcade.color.WHITE, 20,
+            arcade.draw_text("Appuyer sur ESPACE pour commencer", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, arcade.color.WHITE, 20,
                              anchor_x="center")
         elif self.game_state == GameState.GAME_OVER:
-            arcade.draw_text("Game Over! Press SPACE to restart", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+            arcade.draw_text("La partie est terminée! Appuyer sur ESPACE pour commencer une nouvelle partie", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 1.4,
                              arcade.color.WHITE, 20, anchor_x="center")
-        elif self.game_state == GameState.ROUND_ACTIVE and not self.player_attack_chosen:
-            arcade.draw_text("Click to choose your attack: Rock (left), Paper (middle), Scissors (right)",
-                             SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, arcade.color.WHITE, 14, anchor_x="center")
 
     def draw_results(self):
-        result_text = f"Round Result: {self.round_result}"
-        arcade.draw_text(result_text, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, arcade.color.WHITE, 20, anchor_x="center")
-        arcade.draw_text("Press SPACE to continue", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 30, arcade.color.WHITE, 14,
+        # resultat des parties
+        result_text = f"Résultat de la partie: {self.round_result}"
+        arcade.draw_text(result_text, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 100, arcade.color.WHITE, 20,
+                         anchor_x="center")
+        arcade.draw_text("Appuyer sur ESPACE pour continuer", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 70, arcade.color.WHITE, 14,
                          anchor_x="center")
 
     def on_key_press(self, key, key_modifiers):
+        # faire avancer la partie
         if key == arcade.key.SPACE:
             if self.game_state == GameState.NOT_STARTED or self.game_state == GameState.GAME_OVER:
                 self.setup()
@@ -129,25 +166,25 @@ class MyGame(arcade.Window):
                 self.reset_round()
 
     def on_mouse_press(self, x, y, button, key_modifiers):
+        # choisir les attaques
         if self.game_state == GameState.ROUND_ACTIVE and not self.player_attack_chosen:
-            if x < SCREEN_WIDTH // 3:
-                self.player_attack_type = AttackType.ROCK
-            elif x < 2 * SCREEN_WIDTH // 3:
-                self.player_attack_type = AttackType.PAPER
-            else:
-                self.player_attack_type = AttackType.SCISSORS
-
-            self.player_attack_chosen = True
-            self.attack_animation = AttackAnimation(self.player_attack_type)
-            self.attack_animation.center_x = self.PLAYER_IMAGE_X
-            self.attack_animation.center_y = self.PLAYER_IMAGE_Y - 80  # Ajuste pour afficher sous l'image du joueur
-
+            for attack_type, sprite in self.attack_options.items():
+                if sprite.collides_with_point((x, y)):
+                    self.player_attack_type = attack_type
+                    self.player_attack_chosen = True
+                    for other_sprite in self.attack_options.values():
+                        if other_sprite != sprite:
+                            other_sprite.visible = False
+                    self.player_attack_sprite = arcade.Sprite(f"assets/{self.player_attack_type.name.lower()}.png",
+                                                              scale=0.5)
+                    self.player_attack_sprite.center_x = self.PLAYER_IMAGE_X
+                    self.player_attack_sprite.center_y = self.PLAYER_IMAGE_Y - 80
+                    break
 
 def main():
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     game.setup()
     arcade.run()
-
 
 if __name__ == "__main__":
     main()
